b5ed520272058e4aaecdac4bb251163d74d67bb4
sudoaj <salamajayi@yahoo.com>
2023-01-08 12:57:39 -0500

    update

--- musicapp-angular/src/app/app.module.ts
+++ musicapp-angular/src/app/app.module.ts
@@ -1,24 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
-import {LyricsModule} from './lyrics/lyrics.module'
-import {VisualizeModule} from './visualize/visualize.module'
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    HomeModule,
-    SharedModule,
-    BrowserModule,
-    AppRoutingModule,
-    LyricsModule,
-    VisualizeModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
